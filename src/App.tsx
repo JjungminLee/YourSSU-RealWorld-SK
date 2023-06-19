b549b9ee1ffd6e90c83357baa45d0bedd7e68d16
JjungminLee <julie0964@naver.com>
2023-06-20 05:00:15 +0900

    fix:폴더구조 변경 -> components 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 import styled from '@emotion/styled/macro'
 
 function App() {
 
   return (
-    <></>
+    <>
+      <h1>Main</h1>
+    </>
      
   )
 }
