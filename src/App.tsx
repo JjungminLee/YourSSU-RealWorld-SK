c622b2a02585cda6da9e04f174d634c154b0e5ff
JjungminLee <julie0964@naver.com>
2023-06-20 14:45:17 +0900

    feat:user 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import styled from '@emotion/styled/macro'
+import { QueryClientProvider ,QueryClient} from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { RecoilRoot } from 'recoil'
+import { BrowserRouter,Router } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
+import UserLogin from './components/UserLogin.tsx'
+import Home from './components/Home.tsx'
 
+
+const queryClient = new QueryClient();
 function App() {
 
   return (
     <>
-      <h1>Main</h1>
+      <QueryClientProvider client={queryClient}>
+        <StrictMode>
+          <RecoilRoot>
+            <BrowserRouter>
+              <Routes>
+                <Route></Route>
+                <Route path="/login" element={<UserLogin/>}></Route>
+                <Route path="/" element={<Home/>}></Route>
+              </Routes>
+            </BrowserRouter>
+          </RecoilRoot>
+        </StrictMode>
+
+  </QueryClientProvider>
     </>
      
   )
