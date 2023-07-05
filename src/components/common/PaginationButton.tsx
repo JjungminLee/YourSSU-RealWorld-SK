605d10694037b51479aa360f89e9fee0fe66fa88
intersoom <sallybig@naver.com>
2023-07-06 01:23:17 +0900

    feat: 페이지네이션 기능 추가

---
+++ src/components/common/PaginationButton.tsx
@@ -0,0 +1,23 @@
+import { useRecoilState } from 'recoil';
+import { pageState } from '@src/types/pageState';
+
+type ButtonProps = {
+  btn: string;
+  pageNum: number;
+  current: boolean;
+};
+
+export default function PaginationButton({ btn, pageNum, current }: ButtonProps) {
+  const [, setPage] = useRecoilState(pageState);
+  const handleClick = () => {
+    setPage(pageNum);
+  };
+
+  return (
+    <li className={`page-item ng-scope ng-class ${current && `active`}`}>
+      <a className="page-link ng-binding" onClick={handleClick} href="#">
+        {btn}
+      </a>
+    </li>
+  );
+}
